5f53d7ca9f85e56933d280b7bc824b49fce88019
Kent Boston <kentcboston@gmail.com>
2024-10-04 06:45:58 +0800

    Categorical Data Description

--- ga3.py
+++ ga3.py
@@ -41,6 +41,45 @@ st.text(df_info_as_string)
 st.write("**Show missing values or sum of null values per column**")
 st.write(df.isna().sum())
 
-# Generate statistical metrics excluding non-numeric columns
+# Numerical Data Description
 st.markdown("## **`Numerical Data Description`**")
 st.write(df.describe())
+
+# Categorical Data Description
+st.markdown("## **`Categorical Data Description`**")
+
+# Categorical Data: Company
+st.write("**Laptop manufacturer count sorted in descending order**")
+st.write(df['Company'].value_counts())
+
+# Categorical Data: Product
+st.write("**Brand and Model count sorted in descending order**")
+st.write(df['Product'].value_counts())
+
+# Categorical Data: TypeName
+st.write("**Laptop Type count sorted in descending order**")
+st.write(df['TypeName'].value_counts())
+
+# Categorical Data: CPU_Company
+st.write("**Central Processing Unit manufacturer count sorted in descending order**")
+st.write(df['CPU_Company'].value_counts())
+
+# Categorical Data: CPU_Type
+st.write("**Central Processing Unit type count sorted in descending order**")
+st.write(df['CPU_Type'].value_counts())
+
+# Categorical Data: Memory
+st.write("**Hard Disk / SSD Memory count sorted in descending order**")
+st.write(df['Memory'].value_counts())
+
+# Categorical Data: GPU_Company
+st.write("**Graphics Processing Unit manufacturer count sorted in descending order**")
+st.write(df['GPU_Company'].value_counts())
+
+# Categorical Data: GPU_Type
+st.write("**Graphics Processing Unit type count sorted in descending order**")
+st.write(df['GPU_Type'].value_counts())
+
+# Categorical Data: OpSys
+st.write("**Operating System count sorted in descending order**")
+st.write(df['OpSys'].value_counts())
