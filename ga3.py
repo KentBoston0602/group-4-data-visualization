5bc0fe597be405b53fd33add3f8a423c304a5c91
Kent Boston <kentcboston@gmail.com>
2024-10-04 05:19:00 +0800

    added title, subheader and member names

--- ga3.py
+++ ga3.py
@@ -4,3 +4,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
+
+st.title("Group 4_CSS145_BM7")
+st.subheader("Dataset: Laptop Price Dataset")
+
+members = [
+    "Kent Patrick **BOSTON**",
+    "Luis Frederick **CONDA**",
+    "Chaze Kyle **FIDELINO**",
+    "Joseph Isaac **ZAMORA**"
+]
+
+st.write("Members:")
+for member in members:
+    st.markdown(f"* {member}")
